d3c43a42e25b1042e65283408f02253ca28b53dc
Blake Burnette <bburnette@regencyop.com>
2015-12-31 11:25:17 -0500

    Repurposing BaseTheme2

---
+++ app/js/directives/tree.js
@@ -0,0 +1,32 @@
+four51.app.directive('categorytree', function() {
+	var obj = {
+		restrict: 'E',
+		replace: true,
+		scope: {
+			tree: '=',
+            current: '='
+		},
+		templateUrl: 'partials/categoryTree.html'
+	};
+	return obj;
+});
+
+four51.app.directive('node', ['$compile', function($compile) {
+	var obj = {
+		restrict: 'E',
+		replace: true,
+		scope: {
+			node: '=',
+            current: '='
+		},
+		template: '<li class="451_cat_item dropdownColor" ng-class="{\'active\':  current.InteropID == node.InteropID}"><a ng-href="catalog/{{node.InteropID}}" ng-bind-html="node.Name"class="dropdownColor"></a></li>',
+		link: function(scope, element) {
+			if (angular.isArray(scope.node.SubCategories)) {
+			    //replace Categories with SubCategories for full list
+				element.append("<categorytree class='colorControl' tree='node.Categories' current='current'/>");
+				$compile(element.contents())(scope);
+			}
+		}
+	};
+	return obj;
+}]);
